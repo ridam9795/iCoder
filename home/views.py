bc590140c806e8ff7f61a5ba952bd55c922bf15a
Ridam Nagar <ridamnagar11@gmail.com>
2023-02-17 15:57:27 +0530

    Added authentication feature

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 from home.models import Contact
 from blog.models import Post
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -48,3 +48,58 @@ def search(request):
                 request, "No search results found. Please refine your query.")
     context = {"allPosts": allPosts, "query": query}
     return render(request, 'home/search.html', context)
+
+
+def handleSignup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        if len(username) < 10:
+            messages.error(
+                request, " Your user name must be under 10 characters")
+            return redirect('home')
+
+        if not username.isalnum():
+            messages.error(
+                request, " User name should only contain letters and numbers")
+            return redirect('home')
+        if (pass1 != pass2):
+            messages.error(request, " Passwords do not match")
+            return redirect('home')
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(
+            request, " Your iCoder account has been successfully created")
+        return redirect('home')
+
+    else:
+        return HttpResponse("404 - Not Found")
+
+
+def handleLogin(request):
+    if request.method == "POST":
+        loginusername = request.POST['loginusername']
+        loginpassword = request.POST['loginpassword']
+
+        user = authenticate(username=loginusername, password=loginpassword)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid Credentilas, Please try again")
+            return redirect('home')
+    return render(request, 'home/pageNotFound.html')
+
+
+def handleLogout(request):
+    logout(request)
+    messages.success(request, "Successfully Logged Out")
+    return redirect('home')
